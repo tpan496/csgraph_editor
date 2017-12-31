9e3c03eb7f1fa6cf67b15c8265e4347ab49956b0
tpan496 <tianleip@andrew.cmu.edu>
2017-12-31 14:48:07 +0800

    make scaler draggable

--- app/index.js
+++ app/index.js
@@ -1,18 +1,22 @@
 var svg;
+var logger;
 var svgns = "http://www.w3.org/2000/svg"
 var selectedElement = 0;
+var selectedScaler = 0;
 var mouseOverNode = false;
 var currentX = 0;
 var currentY = 0;
 var mouseX = 0;
 var mouseY = 0;
-var lastX = 0;
-var lastY = 0;
 var currentMatrix = [1, 0, 0, 1, 0, 0];
 var id = 0;
+var dragging = false;
+var lastX = 0;
+var lastY = 0;
 
 $(document).ready(function () {
     svg = $('svg')[0];
+    logger = $('#logger');
     var spawnCircleButton = $('#button-circle');
     spawnCircleButton.click(function () {
         var rx = randomRange(0, 20);
@@ -21,60 +25,66 @@ $(document).ready(function () {
         var y = svg.getBoundingClientRect().height / 2 + ry;
         var node = drawNode(x, y);
         svg.appendChild(node);
-        if(selectedElement!=0){
-           turnOffSelectedElementScaler();
+        if (selectedElement != 0) {
+            turnOffSelectedElementScaler();
         }
         selectedElement = node;
     });
-    $('#paper').click(function (){
-        if(selectedElement != 0 && !mouseOverNode){
+    $('#paper').click(function () {
+        if (selectedElement != 0 && !mouseOverNode) {
             turnOffSelectedElementScaler();
         }
     });
 });
 
 var turnOffSelectedElementScaler = function () {
-    var scaler = selectedElement.children[1];
-    scaler.setAttributeNS(null, "visibility", "hidden");
+    if (isNode(selectedElement)) {
+        var scaler = selectedElement.children[1];
+        scaler.setAttribute("visibility", "hidden");
+    }
 };
 
-var drawNode = function(x,y) {
+var drawNode = function (x, y) {
     var node = document.createElementNS(svgns, 'g');
-    var circle = drawCircle(x,y,20,'white','black',1.5);
-    var mutator = drawMutator(x,y);
+    var circle = drawCircle(x, y, 20, 'white', 'black', 1.5);
+    var mutator = drawMutator(x, y, 20);
     node.appendChild(circle);
     node.appendChild(mutator);
-    node.setAttribute('class', 'draggable');
     node.setAttribute('transform', 'matrix(1 0 0 1 0 0)');
     node.setAttribute('onmousedown', 'selectElement(evt)');
-    node.setAttribute('onclick','onClickElement(evt)');
-    node.setAttribute('id', 'node');
-    node.setAttribute('onmouseover', "hoverElement(evt)");
-    node.setAttribute('onmouseout',"outElement(evt)");
+    node.setAttribute('class', 'node');
+    node.setAttribute('onmouseover', 'hoverElement(evt)');
+    node.setAttribute('onmouseout', 'outElement(evt)');
+    node.setAttribute('radius', 20);
     id += 1;
     return node;
 };
 
-var drawMutator = function(x,y) {
+var drawMutator = function (x, y, radius) {
     var mutator = document.createElementNS(svgns, 'g');
-    var scaler = drawScaler(x,y);
-    var linker = drawLinker(x,y);
+    var scaler = drawScaler(x, y, radius);
+    var linker = drawLinker(x, y);
     mutator.appendChild(scaler);
     mutator.appendChild(linker);
-    mutator.setAttribute('id','mutator');
+    mutator.setAttribute('id', 'mutator');
     return mutator;
-}
+};
 
-var drawScaler = function(x,y) {
+var drawLinker = function (x, y) {
+    var linker = drawCircle(x, y, 4, 'rgb(0,255,0)', 'black', 0.5);
+    return linker;
+};
+
+var drawScaler = function (x, y, radius) {
     var scaler = document.createElementNS(svgns, 'g');
-    var scaleCircle1 = drawCircle(x+20,y+20,4,'rgb(0,122,255)', 'white',0.5);
-    var scaleCircle2 = drawCircle(x-20,y-20,4,'rgb(0,122,255)', 'white',0.5);
-    var scaleCircle3 = drawCircle(x+20,y-20,4,'rgb(0,122,255)', 'white',0.5);
-    var scaleCircle4 = drawCircle(x-20,y+20,4,'rgb(0,122,255)', 'white',0.5);
-    var line1 = drawDashedLine(x-20,y-20,x+20,y-20,[2,2],2,'rgb(0,122,255)');
-    var line2 = drawDashedLine(x-20,y-20,x-20,y+20,[2,2],2,'rgb(0,122,255)');
-    var line3 = drawDashedLine(x+20,y-20,x+20,y+20,[2,2],2,'rgb(0,122,255)');
-    var line4 = drawDashedLine(x-20,y+20,x+20,y+20,[2,2],2,'rgb(0,122,255)');
+    var scaleCircle1 = drawScalerNode(x + radius, y + radius);
+    var scaleCircle2 = drawScalerNode(x - radius, y - radius);
+    var scaleCircle3 = drawScalerNode(x + radius, y - radius);
+    var scaleCircle4 = drawScalerNode(x - radius, y + radius);
+    var line1 = drawDashedLine(x - radius, y - radius, x + radius, y - radius, [2, 2], 2, 'rgb(0,122,255)');
+    var line2 = drawDashedLine(x - radius, y - radius, x - radius, y + radius, [2, 2], 2, 'rgb(0,122,255)');
+    var line3 = drawDashedLine(x + radius, y - radius, x + radius, y + radius, [2, 2], 2, 'rgb(0,122,255)');
+    var line4 = drawDashedLine(x - radius, y + radius, x + radius, y + radius, [2, 2], 2, 'rgb(0,122,255)');
     scaler.appendChild(scaleCircle1);
     scaler.appendChild(scaleCircle2);
     scaler.appendChild(scaleCircle3);
@@ -87,78 +97,95 @@ var drawScaler = function(x,y) {
     return scaler;
 };
 
-var drawLinker = function(x,y) {
-    var linker = drawCircle(x,y,4,'rgb(0,255,0)', 'black', 0.5);
-    return linker;
+var drawScalerNode = function (x, y) {
+    var scaleCircle = drawCircle(x, y, 5, 'rgb(0,122,255)', 'white', 0.5);
+    scaleCircle.setAttribute('onmousedown', 'selectScaler(evt)');
+    scaleCircle.setAttribute('class', 'scale-node');
+    scaleCircle.setAttribute('transform', 'matrix(1 0 0 1 0 0)');
+    return scaleCircle;
 };
 
-var drawCircle = function (x, y, radius, fill, stroke, strokeWidth) {
-    var circle = document.createElementNS(svgns, 'circle');
-    circle.setAttributeNS(null, 'cx', x);
-    circle.setAttributeNS(null, 'cy', y);
-    circle.setAttributeNS(null, 'r', radius);
-    circle.setAttributeNS(null, 'fill', fill);
-    circle.setAttributeNS(null, 'stroke', stroke);
-    circle.setAttributeNS(null, 'stroke-width', strokeWidth);
-    return circle;
+var selectScaler = function (e) {
+    e.preventDefault();
+    if (e.target.getAttribute("class") != "scale-node") {
+        return;
+    }
+    dragging = true;
+    logger.html(e.clientX);
+    var target = e.target;
+    selectedScaler = target;
+    currentX = e.clientX;
+    currentY = e.clientY;
+    currentMatrix = e.target.getAttribute("transform").slice(7, -1).split(' ');
+    for (var i = 0; i < currentMatrix.length; i++) {
+        currentMatrix[i] = parseFloat(currentMatrix[i]);
+    }
+    svg.removeAttribute("onmousemove");
+    svg.removeAttribute("onmouseup");
+    svg.removeAttribute("onmouseout");
+    svg.setAttribute('onmousemove', 'moveScaler(evt)');
+    svg.setAttribute('onmouseup', 'deselectElement(evt)')
 };
 
-var drawRect = function (x,y,w,h,fill,id) {
-    var rect = document.createElementNS(svgns, 'rect');
-    rect.setAttributeNS(null, 'x', x);
-    rect.setAttributeNS(null, 'y', y);
-    rect.setAttributeNS(null, 'width', w);
-    rect.setAttributeNS(null, 'height', h);
-    rect.setAttributeNS(null, 'fill', fill);
-    rect.setAttributeNS(null, 'id', id);
-    return rect;
-};
+var moveScaler = function (e) {
+    e.preventDefault();
+    var dx = e.clientX - currentX;
+    var dy = e.clientY - currentY;
+    currentMatrix[4] += dx;
+    currentMatrix[5] += dy;
+    newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
 
-var drawLine = function (x1,y1,x2,y2,w,color) {
-    var line = document.createElementNS(svgns, 'line');
-    line.setAttribute('x1',x1);
-    line.setAttribute('x2',x2);
-    line.setAttribute('y1',y1);
-    line.setAttribute('y2',y2);
-    line.setAttribute('stroke',color);
-    line.setAttribute('stroke-width',w);
-    return line;
-};
-
-var drawDashedLine = function (x1,y1,x2,y2,array,w,color) {
-    var line = document.createElementNS(svgns, 'line');
-    line.setAttribute('x1',x1);
-    line.setAttribute('x2',x2);
-    line.setAttribute('y1',y1);
-    line.setAttribute('y2',y2);
-    line.setAttribute('stroke',color);
-    line.setAttribute('stroke-width',w);
-    line.setAttribute('stroke-dasharray',array)
-    return line;
+    selectedScaler.setAttribute('transform', newMatrix);
+    currentX = e.clientX;
+    currentY = e.clientY;
+    scale(selectedElement, dx, dy);
 };
 
 var selectElement = function (e) {
     e.preventDefault();
-    if(e.target.parentElement.getAttribute("id") != "node"){
+    if (e.target.parentElement.getAttribute("class") != "node") {
         return;
     }
-    if(selectedElement != e.target.parentElement) {
+    if (selectedElement != e.target.parentElement) {
         turnOffSelectedElementScaler();
     }
     selectedElement = e.target.parentElement;
     var scaler = selectedElement.children[1];
-    if(scaler.getAttribute("visibility") === "hidden"){
+    if (scaler.getAttribute("visibility") === "hidden") {
         scaler.setAttribute("visibility", "visible");
     }
     currentX = e.clientX;
     currentY = e.clientY;
-    currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7, -1).split(' ');
+    currentMatrix = selectedElement.getAttribute("transform").slice(7, -1).split(' ');
     for (var i = 0; i < currentMatrix.length; i++) {
         currentMatrix[i] = parseFloat(currentMatrix[i]);
     }
 
     svg.setAttribute("onmousemove", "moveElement(evt)");
     svg.setAttribute("onmouseup", "deselectElement(evt)");
+};
+
+var moveElement = function (e) {
+    e.preventDefault();
+    var dx = e.clientX - currentX;
+    var dy = e.clientY - currentY;
+    currentMatrix[4] += dx;
+    currentMatrix[5] += dy;
+    newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
+
+    selectedElement.setAttribute('transform', newMatrix);
+    currentX = e.clientX;
+    currentY = e.clientY;
+};
+
+var deselectElement = function (evt) {
+    evt.preventDefault();
+    svg.removeAttribute("onmousemove");
+    svg.removeAttribute("onmouseup");
+};
+
+var randomRange = function (min, max) {
+    return Math.random() * (max - min) + min;
 };
 
 var hoverElement = function (e) {
@@ -169,41 +196,17 @@ var outElement = function (e) {
     mouseOverNode = false;
 };
 
-var moveElement = function (e) {
-    e.preventDefault();
-    dx = lastX + e.clientX - currentX;
-    dy = lastY + e.clientY - currentY;
-    currentMatrix[4] += dx;
-    currentMatrix[5] += dy;
-    newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
-
-    selectedElement.setAttributeNS(null, "transform", newMatrix);
-    currentX = e.clientX;
-    currentY = e.clientY;
+var isNode = function (e) {
+    if (e.getAttribute('class') == 'node') {
+        return true;
+    } else {
+        return false;
+    }
 };
 
-var onClickElement = function (e) {
-    e.preventDefault();
-    if(e.target.parentElement.getAttribute('id')!="node"){
+var scale = function (selectedElement, dx, dy) {
+    if (!isNode(selectedElement)) {
         return;
     }
-    var scaler = selectedElement.children[1];
-    if(scaler.getAttribute("visibility") === "hidden"){
-        selectedElement = e.target.parentElement;
-        scaler.setAttribute("visibility", "visible");
-    }
-    $('#selected').text(e.target.getAttributeNS(null, 'id'));
-};
-
-var deselectElement = function (evt) {
-    evt.preventDefault();
-    lastX = 0;
-    lastY = 0;
-    svg.removeAttribute("onmousemove");
-    svg.removeAttribute("onmouseout");
-    svg.removeAttribute("onmouseup");
-};
-
-var randomRange = function(min, max) {
-    return Math.random() * (max - min) + min;
+    var radius = selectedElement.getAttribute('radius');
 };
---
+++ app/shape.js
@@ -0,0 +1,45 @@
+var svgns = "http://www.w3.org/2000/svg"
+var drawCircle = function (x, y, radius, fill, stroke, strokeWidth) {
+    var circle = document.createElementNS(svgns, 'circle');
+    circle.setAttributeNS(null, 'cx', x);
+    circle.setAttributeNS(null, 'cy', y);
+    circle.setAttributeNS(null, 'r', radius);
+    circle.setAttributeNS(null, 'fill', fill);
+    circle.setAttributeNS(null, 'stroke', stroke);
+    circle.setAttributeNS(null, 'stroke-width', strokeWidth);
+    return circle;
+};
+
+var drawRect = function (x,y,w,h,fill,id) {
+    var rect = document.createElementNS(svgns, 'rect');
+    rect.setAttributeNS(null, 'x', x);
+    rect.setAttributeNS(null, 'y', y);
+    rect.setAttributeNS(null, 'width', w);
+    rect.setAttributeNS(null, 'height', h);
+    rect.setAttributeNS(null, 'fill', fill);
+    rect.setAttributeNS(null, 'id', id);
+    return rect;
+};
+
+var drawLine = function (x1,y1,x2,y2,w,color) {
+    var line = document.createElementNS(svgns, 'line');
+    line.setAttribute('x1',x1);
+    line.setAttribute('x2',x2);
+    line.setAttribute('y1',y1);
+    line.setAttribute('y2',y2);
+    line.setAttribute('stroke',color);
+    line.setAttribute('stroke-width',w);
+    return line;
+};
+
+var drawDashedLine = function (x1,y1,x2,y2,array,w,color) {
+    var line = document.createElementNS(svgns, 'line');
+    line.setAttribute('x1',x1);
+    line.setAttribute('x2',x2);
+    line.setAttribute('y1',y1);
+    line.setAttribute('y2',y2);
+    line.setAttribute('stroke',color);
+    line.setAttribute('stroke-width',w);
+    line.setAttribute('stroke-dasharray',array)
+    return line;
+};
